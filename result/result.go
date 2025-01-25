29b072bc964ffe37b8b9310609dbe5eb18ab82f3
gildemberg-santos <gildemberg.santos@gmail.com>
2025-01-25 15:35:10 -0300

    adiciona métodos de criação e manipulação à estrutura Result, incluindo Bind, Fmap, ValueOr e ToMaybe

--- result/result.go
+++ result/result.go
@@ -1,8 +1,24 @@
 package result
 
+import "github.com/gildemberg-santos/monads-go/maybe"
+
 type Result struct {
 	isSuccess bool
 	value     interface{}
+}
+
+func NewSuccess(value interface{}) *Result {
+	return &Result{
+		isSuccess: true,
+		value:     value,
+	}
+}
+
+func NewFailure(value interface{}) *Result {
+	return &Result{
+		isSuccess: false,
+		value:     value,
+	}
 }
 
 func (r *Result) IsSuccess() bool {
@@ -13,39 +29,69 @@ func (r *Result) IsFailure() bool {
 	return !r.isSuccess
 }
 
-func (r *Result) GetSuccess() interface{} {
-	if r.isSuccess {
+func (r *Result) GetValue() interface{} {
+	return r.value
+}
+
+func (r *Result) Bind(f func(interface{}) *Result) *Result {
+	if r.IsSuccess() {
+		return f(r.value)
+	}
+	return r
+}
+
+func (r *Result) Fmap(f func(interface{}) interface{}) *Result {
+	if r.IsSuccess() {
+		return NewSuccess(f(r.value))
+	}
+	return r
+}
+
+func (r *Result) ValueOr(defaultValue interface{}) interface{} {
+	if r.IsSuccess() {
 		return r.value
 	}
-	return nil
+	return defaultValue
 }
 
-func (r *Result) GetFailure() interface{} {
-	if !r.isSuccess {
+func (r *Result) Value() interface{} {
+	if r.IsSuccess() {
 		return r.value
 	}
-	return nil
+	panic("value! was called on Failure")
 }
 
-func NewSuccess(values ...interface{}) *Result {
-	return &Result{
-		isSuccess: true,
-		value:     valueOf(values...),
+func (r *Result) Or(other *Result) *Result {
+	if r.IsSuccess() {
+		return r
 	}
+	return other
 }
 
-func NewFailure(values ...interface{}) *Result {
-	return &Result{
-		isSuccess: false,
-		value:     valueOf(values...),
+func (r *Result) Failure() interface{} {
+	if r.IsFailure() {
+		return r.value
 	}
+	panic("failure was called on Success")
 }
 
-func valueOf(values ...interface{}) interface{} {
-	if len(values) == 1 {
-		return values[0]
-	} else if len(values) > 1 {
-		return values
+func (r *Result) ToMaybe() *maybe.Maybe {
+	if r.IsSuccess() {
+		return maybe.NewSome(r.value)
 	}
-	return nil
+	return maybe.NewNone()
+}
+
+func (r *Result) Either(successFunc, failureFunc func(interface{}) interface{}) interface{} {
+	if r.IsSuccess() {
+		return successFunc(r.value)
+	}
+	return failureFunc(r.value)
+}
+
+func (r *Result) AltMap(f func(interface{}) interface{}) *Result {
+	if r.IsFailure() {
+		return NewFailure(f(r.value))
+	}
+	return r
 }
